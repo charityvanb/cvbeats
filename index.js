2903c37a7a109361608072275a02abb2f8df62bf
charityvanb <charityvanb@gmail.com>
2018-10-03 22:24:48 -0600

    Added event listeners and populated the order box.

--- index.js
+++ index.js
@@ -1,38 +1,70 @@
+var orderItem = ''
 fetch('https://galvanize-eats-api.herokuapp.com/menu')
+
+
     .then(function(response) {
         return response.json()
     })
     .then(function(data) {
-        var select = document.querySelector('.dropdown-menu')
+        var select = document.querySelector('#selectBar')
         for (var i = 0; i < data.menu.length; i++) {
-            var choice = document.createElement('a')
+            var choice = document.createElement('option')
             choice.innerText = data.menu[i].name + " $" + data.menu[i].price
             choice.setAttribute('value', data.menu[i].name)
             choice.classList.add("dropdown-item")
             select.appendChild(choice)
-            choice.addEventListener('click', arrayAdd)
+            // choice.addEventListener('change', arrayAdd)
             // add a value to each menu item
+
         }
-    })
-var chosenSnacks = []
-var option = document.querySelectAll(option)
-
-function arrayAdd() {
-   option.innerText + " " + chosenSnacks.push(event.target.innerText)
+        var select = document.querySelector('#selectBar')
+        select.addEventListener('change', function(event){
+            orderItem = document.querySelector('#selectBar option:checked').innerText
+        
+        })
     
- }
-//make function here
+    })
 
-// function populateList() {
-//     console.log(event.target.innerText)
-//     var orderBox = document.querySelector('.orderbox')
-//     var orderItem = document.createElement('p')
-//     orderItem.innerText = event.target.innerText
-//     orderBox.appendChild(orderItem)
-//     // add a value and pull the value in here
-//     // orderBox = appendChild(orderItem)
+var itemQuantity = ''
+var quantity = document.querySelector('#quant')
+quantity.addEventListener('change', function(event){
+    itemQuantity = document.querySelector('#quant option:checked').innerText
+})
 
+var addItem = document.querySelector("#addItem")
+addItem.addEventListener('click', populateList)
+
+function populateList() {
+    var orderBox = document.querySelector('.orderbox')
+    var myOrder = document.createElement('p')
+    orderBox.appendChild(myOrder)
+    myOrder.innerText = itemQuantity + " x " + orderItem
+
+}
+    
+// function selectItem() {
+//     event.target.setAttribute("class", "frog")
+//     console.log(event.target)
 // }
+
+// var options = document.querySelectAll('option')
+
+// function arrayAdd() {
+    
+//     console.log(event.target)
+    
+//  }
+
+//  var addItem = document.querySelector('#addItem')
+
+//  addItem.addEventListener('click', valueAdd)
+
+//  function valueAdd() {
+//      var quantity = option.selected.innerHTML
+//      console.log(quantity)
+//  }
+
+
 
 
 
